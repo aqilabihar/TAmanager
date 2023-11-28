cd6939451b25d1adfb539f21fd5ca207398c9dfd
aqilabihar <60000505+aqilabihar@users.noreply.github.com>
2023-11-28 10:59:51 +0700

    repair

--- TAmanager/menuMHS.vb
+++ TAmanager/menuMHS.vb
@@ -7,35 +7,5 @@
         Panel1.Controls.Add(panel)
         panel.Show()
     End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        switchppanel(Form3)
-    End Sub
 
-    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        switchppanel(Form4)
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        switchppanel(Form5)
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        switchppanel(Form6)
-    End Sub
-
-    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        switchppanel(Form7)
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        switchppanel(Form8)
-    End Sub
-
-    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-        switchppanel(Form9)
-    End Sub
-
-    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        switchppanel(Form10)
-    End Sub
 End Class
